2ce66421225ba874af027d68a0e2515ba457ae97
nkokhelox <nkokhelox@gmail.com>
2017-04-29 19:55:01 +0200

    Origin commit.

---
+++ src/main/java/pisd/works/InstanceDiff.java
@@ -0,0 +1,176 @@
+package pisd.works;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Determines the differences between the fields of the 2 instances of the same class.
+ * First check if instances are equal.
+ * Check if all fields are the same by traversing though the inheritance hierarchy.
+ * Compare the field values using the normal equality check.
+ *
+ * {@author nkokhelox <Nkokhelo E. Mhlongo>}
+ */
+public class InstanceDiff
+{
+    private final Object instance1, instance2;
+    private final int climbLevel;
+
+    private enum DiffStratergy
+    {
+        EXCLUDE_FIELDS, DIFF_OF_FIELDS, ALL_FIELDS
+    }
+
+    private class DiffDecision
+    {
+        Set<String> fieldNameSet; DiffStratergy fieldSetUsage;
+
+        DiffDecision(DiffStratergy fieldSetUsage)
+        {
+            this(fieldSetUsage, null);
+        }
+
+        DiffDecision(DiffStratergy fieldSetUsage, Set<String> fieldNameSet)
+        {
+            this.fieldNameSet = fieldNameSet;
+            this.fieldSetUsage = fieldSetUsage;
+        }
+        boolean continueFieldDiff(String fieldName)
+        {
+            return fieldNameSet == null || fieldSetUsage == DiffStratergy.ALL_FIELDS ||
+                    (fieldSetUsage == DiffStratergy.EXCLUDE_FIELDS && !fieldNameSet.contains(fieldName)) ||
+                    (fieldSetUsage == DiffStratergy.DIFF_OF_FIELDS && fieldNameSet.contains(fieldName));
+        }
+        boolean continueFieldDiff(Class klass, int currentLevel){
+            return !(Object.class.equals(klass)) && (currentLevel <= climbLevel || climbLevel < 0);
+        }
+    }
+
+    public static class FieldDiff
+    {
+        private final String fieldName;
+        private final Object inst1FieldValue;
+        private final Object inst2FieldValue;
+
+        public FieldDiff(String fieldName, Object inst1FieldValue, Object inst2FieldValue)
+        {
+            this.fieldName = fieldName;
+            this.inst1FieldValue = inst1FieldValue;
+            this.inst2FieldValue = inst2FieldValue;
+        }
+
+        public String getFieldName()
+        {
+            return fieldName;
+        }
+
+        public Object getInst1FieldValue()
+        {
+            return inst1FieldValue;
+        }
+
+        public Object getInst2FieldValue()
+        {
+            return inst2FieldValue;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return super.hashCode() + (fieldName != null ? fieldName.hashCode() : 0);
+        }
+    }
+
+    /**
+     * All fields all classes
+     *
+     * @param instance1 first instance
+     * @param instance2 second instance
+     * @return the {@code non-null} {@link Set}&lt;{@link FieldDiff}&gt; for the fields with different values.
+     * @since Origin
+     */
+    public InstanceDiff(Object instance1, Object instance2)
+    {
+        this(instance1, instance2, -1);
+    }
+
+    /**
+     * @param instance1  first instance
+     * @param instance2  second instance
+     * @param climbLevel {@code < 0} go through every super class,
+     *                   {@code = 0} don't include super classes fields,
+     *                   {@code >= 1} include fields from n super classes. e.g.
+     *                   <i>if {@code climbLevel = 5} then include field of up to 5 super classes.</i>
+     * @since Origin
+     */
+    public InstanceDiff(Object instance1, Object instance2, int climbLevel)
+    {
+        this.instance1 = instance1;
+        this.instance2 = instance2;
+        this.climbLevel = climbLevel;
+    }
+
+    /**
+     * @param ignoreFields a set of field names to be excluded/ignored when doing instance diff.
+     * @return the {@code non-null} {@link Set}&lt;{@link FieldDiff}&gt; for the fields with different values.
+     * @since Origin
+     */
+    public Set<FieldDiff> getDiffExcluding(Set<String> ignoreFields) throws Exception
+    {
+        return getDiff(new DiffDecision(DiffStratergy.EXCLUDE_FIELDS, ignoreFields));
+    }
+
+    /**
+     * @param fieldsToDiff a set of field names to be interrogated for differences from the provided instances.
+     * @return the {@code non-null} {@link Set}&lt;{@link FieldDiff}&gt; for the fields with different values.
+     * @since Origin
+     */
+    public Set<FieldDiff> getDiffOf(Set<String> fieldsToDiff) throws Exception
+    {
+        return getDiff(new DiffDecision(DiffStratergy.DIFF_OF_FIELDS, fieldsToDiff));
+    }
+
+    /**
+     * @return the {@code non-null} {@link Set}&lt;{@link FieldDiff}&gt; for the fields with different values.
+     * @since Origin
+     */
+    public Set<FieldDiff> getDiffOf() throws Exception
+    {
+        return getDiff(new DiffDecision(DiffStratergy.ALL_FIELDS));
+    }
+
+
+    private Set<FieldDiff> getDiff(DiffDecision decider) throws Exception
+    {
+        Set diff = new HashSet();
+        if(instance1 != null && instance2 != null && instance1.equals(instance2))
+            {
+            int hierachyLevel = 0;
+            Class klass = instance1.getClass();
+            if(klass == instance2.getClass())
+                {
+                while(decider.continueFieldDiff(klass, hierachyLevel))
+                    {
+                    for(Field field : klass.getDeclaredFields())
+                        {
+                        field.setAccessible(true);
+                        String fieldName = field.getName();
+                        if(decider.continueFieldDiff(fieldName))
+                            {
+                            Object inst1Fv = field.get(instance1);
+                            Object inst2Fv = field.get(instance2);
+                            if(!inst1Fv.equals(inst2Fv))
+                                {
+                                diff.add(new FieldDiff(fieldName, inst1Fv, inst2Fv));
+                                }
+                            }
+                        }
+                    klass = klass.getSuperclass();
+                    hierachyLevel++;
+                    }
+                }
+            }
+        return diff;
+    }
+}
---
+++ src/test/java/pisd/works/InstanceDiffTest.java
@@ -0,0 +1,30 @@
+package pisd.works;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nkokhelox on 2017/04/29.
+ */
+public class InstanceDiffTest
+{
+    @org.junit.Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @org.junit.Test
+    public void getDiffExcluding() throws Exception
+    {
+    }
+
+    @org.junit.Test
+    public void getDiffOf() throws Exception
+    {
+    }
+
+    @org.junit.Test
+    public void getDiff() throws Exception
+    {
+    }
+
+}
